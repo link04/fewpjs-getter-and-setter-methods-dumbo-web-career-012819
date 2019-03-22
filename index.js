d8ae3fa7a114585b398c54cc96509fdf4e2c48b7
Maximo Bautista <max.bautista97@gmail.com>
2019-03-22 14:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ class Circle {
   
   
   get diameter(){
-    return this.radius * 2
+    return this.radius * 2;
   }
   
   get circumference(){
-    return Math.PI * this.diameter
+    return Math.PI * this.diameter;
   }
   
   get area(){
